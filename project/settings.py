b4c8e7b9aac8142a0c332a80fbe4ecf590bf8107
Tejas Arlimatti <tejasarlimatti@gmail.com>
2019-12-08 12:01:33 +0530

    heroku db

--- project/settings.py
+++ project/settings.py
@@ -85,6 +85,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # }
 
 import dj_database_url
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
