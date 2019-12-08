362b18f088ef928fbb0b4de85e9f89b1fd4c8fc7
Tejas Arlimatti <tejasarlimatti@gmail.com>
2019-12-08 11:58:28 +0530

    Added heroku postgres

--- project/settings.py
+++ project/settings.py
@@ -73,16 +73,19 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-DATABASES = {
-    'default': {
-        'ENGINE'  : 'django.db.backends.postgresql',
-        'NAME'    : 'test',
-        'USER'    : 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST'    : '127.0.0.1',
-        'PORT'    : '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE'  : 'django.db.backends.postgresql',
+#         'NAME'    : 'test',
+#         'USER'    : 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST'    : '127.0.0.1',
+#         'PORT'    : '5432',
+#     }
+# }
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
